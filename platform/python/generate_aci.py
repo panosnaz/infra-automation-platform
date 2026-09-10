352270c98e76fd45d8c1d011732298eb688bff31
KAFANTARIS George <KafantarisG@unisystems.gr>
2026-09-10 11:49:11 +0300

    chore: migrate Nautobot host ports

--- platform/python/generate_aci.py
+++ platform/python/generate_aci.py
@@ -44,7 +44,7 @@ sys.path.insert(0, str(_HERE))
 from generator.client import NautobotClient
 from generator.transformer import build_netascode_yaml
 
-_DEFAULT_URL = "http://localhost:8081"
+_DEFAULT_URL = "http://localhost:8080"
 # Default output: platform/netascode/aci/ (sibling of platform/python/)
 _DEFAULT_OUTPUT = _HERE.parent / "netascode" / "aci"
 
